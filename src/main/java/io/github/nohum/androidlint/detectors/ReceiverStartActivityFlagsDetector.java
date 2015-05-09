cd4cc81f053140924f7041507e949d94d140aa01
nohum <eiskalterengel@truthfactory.tk>
2015-05-09 21:36:10 +0200

    added intermediate version of another detector

--- src/main/java/io/github/nohum/androidlint/detectors/ReceiverStartActivityFlagsDetector.java
+++ src/main/java/io/github/nohum/androidlint/detectors/ReceiverStartActivityFlagsDetector.java
@@ -1,6 +1,15 @@
 package io.github.nohum.androidlint.detectors;
 
+import com.android.SdkConstants;
+import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
+import com.android.tools.lint.client.api.JavaParser;
 import com.android.tools.lint.detector.api.*;
+import lombok.ast.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * This detector checks if activities get started with the right flags when being started from broadcast receivers.
@@ -12,15 +21,55 @@ public class ReceiverStartActivityFlagsDetector extends Detector implements Dete
 
     public static final Issue ISSUE = Issue.create(
             "ReceiverStartActivityFlagsMissing",
-            "Naming of resource id is not following conventions",
-            "Resource ids should typically follow the convention of being named in " +
-                    "underscore_casing instead of e.g. camelCasing. This is to maintain " +
-                    "consistency with the rest of the identifiers used by Android.",
+            "Activity is started from withing receiver with wrong flags",
+            "When starting Activities from within broadcast receivers, they must be started with the " +
+            "Intent flag FLAG_ACTIVITY_NEW_TASK as otherwise an exception is thrown.",
             Category.CORRECTNESS,
-            4,
+            8,
             Severity.ERROR,
-            new Implementation(ResourceIdNamingConventionDetector.class, Scope.JAVA_FILE_SCOPE));
+            new Implementation(ReceiverStartActivityFlagsDetector.class, Scope.JAVA_FILE_SCOPE));
 
+    @NonNull
+    @Override
+    public Speed getSpeed() {
+        return Speed.FAST;
+    }
 
+    @Nullable
+    @Override
+    public List<String> applicableSuperClasses() {
+        return Collections.singletonList(SdkConstants.CLASS_BROADCASTRECEIVER);
+    }
 
+    @Override
+    public void checkClass(@NonNull JavaContext context, @Nullable ClassDeclaration declaration, @NonNull Node node,
+                           @NonNull JavaParser.ResolvedClass resolvedClass) {
+        // look for startActivity calls ... if there are none, we immediately done here
+        StartActivityInvocationFinder startActivityVisitor = new StartActivityInvocationFinder();
+        node.accept(startActivityVisitor);
+
+        List<MethodInvocation> startInvocations = startActivityVisitor.getStartInvocs();
+        for (MethodInvocation startInvocation : startInvocations) {
+            // first check every intent for missing flags
+
+            // secondly check if the startActivity gets called by onReceive
+        }
+    }
+
+    private class StartActivityInvocationFinder extends ForwardingAstVisitor {
+        private List<MethodInvocation> invocs = new ArrayList<MethodInvocation>();
+
+        @Override
+        public boolean visitMethodInvocation(MethodInvocation node) {
+            if (node.astName().astValue().startsWith("startActivity")) { // there are several implementations
+                invocs.add(node);
+            }
+
+            return false;
+        }
+
+        public List<MethodInvocation> getStartInvocs() {
+            return invocs;
+        }
+    }
 }
