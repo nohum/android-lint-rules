7419acd5b225a75c2d80058450021bbc5cd944a6
nohum <eiskalterengel@truthfactory.tk>
2015-05-10 00:18:52 +0200

    receiver start activity crash detector finished

--- src/main/java/io/github/nohum/androidlint/detectors/ReceiverStartActivityFlagsDetector.java
+++ src/main/java/io/github/nohum/androidlint/detectors/ReceiverStartActivityFlagsDetector.java
@@ -9,6 +9,7 @@ import lombok.ast.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -23,7 +24,7 @@ public class ReceiverStartActivityFlagsDetector extends Detector implements Dete
             "ReceiverStartActivityFlagsMissing",
             "Activity is started from withing receiver with wrong flags",
             "When starting Activities from within broadcast receivers, they must be started with the " +
-            "Intent flag FLAG_ACTIVITY_NEW_TASK as otherwise an exception is thrown.",
+            "Intent flag `FLAG_ACTIVITY_NEW_TASK` as otherwise an exception is thrown.",
             Category.CORRECTNESS,
             8,
             Severity.ERROR,
@@ -51,8 +52,33 @@ public class ReceiverStartActivityFlagsDetector extends Detector implements Dete
         List<MethodInvocation> startInvocations = startActivityVisitor.getStartInvocs();
         for (MethodInvocation startInvocation : startInvocations) {
             // first check every intent for missing flags
+            String calledMethod = startInvocation.astName().astValue();
+            Expression intentVariable = startInvocation.astArguments().first();
 
-            // secondly check if the startActivity gets called by onReceive
+            // matches e.g. startActivity(new Intent(context, MainActivity.class))
+            if (intentVariable instanceof ConstructorInvocation) {
+                // check if Intent is used: if, this case is wrong and must yield an report!
+                TypeReference ref = ((ConstructorInvocation) intentVariable).astTypeReference();
+
+                if ("Intent".equals(ref.getTypeName())) {
+                    context.report(ISSUE, context.getLocation(startInvocation),
+                            String.format("Activity started with `%s` will probably crash because of missing flag", calledMethod));
+                }
+
+                continue;
+            }
+
+            if (intentVariable instanceof VariableReference) {
+                IntentWriteFinder writeFinder =  new IntentWriteFinder((VariableReference) intentVariable);
+                node.accept(writeFinder);
+
+                if (writeFinder.hasFoundNewTaskFlag()) {
+                    continue;
+                }
+
+                context.report(ISSUE, context.getLocation(startInvocation),
+                        String.format("Activity started with `%s` will probably crash because of missing flag", calledMethod));
+            }
         }
     }
 
@@ -70,6 +96,52 @@ public class ReceiverStartActivityFlagsDetector extends Detector implements Dete
 
         public List<MethodInvocation> getStartInvocs() {
             return invocs;
+        }
+    }
+
+    private class IntentWriteFinder extends ForwardingAstVisitor {
+        private VariableReference callee;
+
+        private boolean foundNewTaskFlag = false;
+
+        public IntentWriteFinder(VariableReference callee) {
+            this.callee = callee;
+        }
+
+        public boolean hasFoundNewTaskFlag() {
+            return foundNewTaskFlag;
+        }
+
+        @Override
+        public void afterVisitMethodInvocation(MethodInvocation node) {
+            if (node.astName().astValue().equals("setFlags")) {
+                if (!(node.astOperand() instanceof VariableReference)) {
+                    return;
+                }
+
+                VariableReference methodCaller = (VariableReference) node.astOperand();
+                // check that call is being made on same Intent variable
+                if (!methodCaller.astIdentifier().astValue().equals(callee.astIdentifier().astValue())) {
+                    return;
+                }
+
+                Iterator<Expression> iter = node.astArguments().iterator();
+                boolean found = false;
+                while (iter.hasNext()) {
+                    Expression expression = iter.next();
+                    if (expression instanceof Select) {
+                        String expressionValue = ((Select) expression).astIdentifier().astValue();
+                        if ("FLAG_ACTIVITY_NEW_TASK".equals(expressionValue)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                // reset the variable to that value as several consecutive setFlags calls may overwrite each other
+                // and we want to respect that...
+                foundNewTaskFlag = found;
+            }
         }
     }
 }
